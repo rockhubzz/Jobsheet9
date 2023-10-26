8291ea0a97420dca471146dc57b6cbb9ed7b66c8
rockhubzz <compatiblewindows7@gmail.com>
2023-10-26 12:39:30 +0700

    Berbagai Modifikasi Percobaan 4

--- ArrayNilai24b.java
+++ ArrayNilai24b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArrayNilai24 {
+public class ArrayNilai24b {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int[] nilaiAkhir= new int[10];
