329019596744f640222492ad49d2a1d40d4fd081
rockhubzz <compatiblewindows7@gmail.com>
2023-10-26 08:08:14 +0700

    Berbagai Modifikasi Percobaan 1

---
+++ ArrayBilangan24b.java
@@ -0,0 +1,14 @@
+public class ArrayBilangan24b {
+    public static void main(String[] args) {
+        int[] bil= new int[4];
+        bil[0]= 5.0;
+        bil[1]= 12867;
+        bil[2]= 7.5;
+        bil[3]= 2000000;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
