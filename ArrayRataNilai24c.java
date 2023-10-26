c235679c94cdb2cd784bd88da79b11f5c19be790
rockhubzz <compatiblewindows7@gmail.com>
2023-10-26 12:23:42 +0700

    Berbagai Modifikasi Percobaan 3

--- ArrayRataNilai24c.java
+++ ArrayRataNilai24c.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class ArrayRataNilai24c {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int[] nilaiMhs= new int[10];
         double total=0, rata2;
         int mhsLulus= 0, mhsGagal=0, nilaiLulus= 0, nilaiGagal= 0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMahasiswa= sc.nextInt();
+        int[] nilaiMhs= new int[jmlMahasiswa];
 
-        for(int i=0; i<nilaiMhs.length; i++){
-            System.out.print("Masukkan nilai mahasiswa ke-"+i+": ");
+        for(int i=0; i<jmlMahasiswa; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
             nilaiMhs[i]= sc.nextInt();
             if(nilaiMhs[i]>70){
                 nilaiLulus+= nilaiMhs[i];
@@ -20,11 +22,11 @@ public class ArrayRataNilai24c {
         }
         System.out.println();
 
-        int rata2Lulus= nilaiLulus/mhsLulus;
-        int rata2Gagal= nilaiGagal/mhsGagal;
-        System.out.println("Rata-rata nilai lulus = "+rata2Lulus);
-        System.out.println("Rata-rata nilai tidak lulus = "+rata2Gagal);
-        System.out.println("Jumlah mahasiswa yang lulus = "+mhsLulus);
-        System.out.println("Jumlah mahasiswa yang tidak lulus = "+mhsGagal);
+        double rata2Lulus= (double) nilaiLulus/mhsLulus;
+        double rata2Gagal= (double) nilaiGagal/mhsGagal;
+        System.out.println("Rata-rata nilai lulus               = "+rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus         = "+rata2Gagal);
+        System.out.println("Jumlah mahasiswa yang lulus         = "+mhsLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus   = "+mhsGagal);
     }
 }
