8dc9b46c4d32f2ca6f422747f014fd0868c80242
rockhubzz <compatiblewindows7@gmail.com>
2023-10-26 08:49:52 +0700

    Berbagai Modifikasi Percobaan 2

---
+++ ArrayNilai24c.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayNilai24c {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[] nilaiAkhir= new int[10];
+
+        for(int i=0; i<nilaiAkhir.length; i++){
+            System.out.print("Masukkan nilai akhir ke-"+i+": ");
+            nilaiAkhir[i]= sc.nextInt();
+        }
+        System.out.println();
+
+        for(int i=0; i<10; i++){
+            if(nilaiAkhir[i]>70){
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+        }
+    }
+}
